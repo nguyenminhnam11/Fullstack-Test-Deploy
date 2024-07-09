e67b4b8396f5e0ae1805ee5e8e8826be44a0fb10
Nam Nguyen <101324693+nguyenminhnam11@users.noreply.github.com>
2024-07-09 14:46:37 +0700

    Update cors.js

--- server/configs/cors.js
+++ server/configs/cors.js
@@ -1,6 +1,6 @@
 const corsOptions = {
     credentials: true,
-    origin: ['https://fullstack-test-deploy-api.vercel.app/'] // Whitelist the domains you want to allow
+    origin: ['https://fullstack-test-deploy.vercel.app/'] // Whitelist the domains you want to allow
 };
 
 export default corsOptions
